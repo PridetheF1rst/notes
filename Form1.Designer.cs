bd1bde6b1cde2b5b053a9d011fc3b0cc869fc9e7
daniil091002 <daniil091002@gmail.com>
2021-12-19 19:58:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace _16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.mnugeneral = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.редактироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.повторитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стеретьВсёToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnugeneral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1067, 526);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // mnugeneral
+            // 
+            this.mnugeneral.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnugeneral.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.редактироватьToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.mnugeneral.Location = new System.Drawing.Point(0, 0);
+            this.mnugeneral.Name = "mnugeneral";
+            this.mnugeneral.Size = new System.Drawing.Size(1067, 28);
+            this.mnugeneral.TabIndex = 1;
+            this.mnugeneral.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuSaveAs,
+            this.toolStripSeparator1,
+            this.mnuExit});
+            this.mnuFile.Image = global::_16.Properties.Resources.загружено;
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(79, 24);
+            this.mnuFile.Text = "Файл";
+            this.mnuFile.Click += new System.EventHandler(this.mnuFile_Click);
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(198, 26);
+            this.mnuNew.Text = "Создать";
+            this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(198, 26);
+            this.mnuOpen.Text = "Открыть..";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(198, 26);
+            this.mnuSave.Text = "Сохранить";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuSaveAs
+            // 
+            this.mnuSaveAs.Name = "mnuSaveAs";
+            this.mnuSaveAs.Size = new System.Drawing.Size(198, 26);
+            this.mnuSaveAs.Text = "Сохранить как..";
+            this.mnuSaveAs.Click += new System.EventHandler(this.mnuSaveAs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(195, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(198, 26);
+            this.mnuExit.Text = "Выход";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // редактироватьToolStripMenuItem
+            // 
+            this.редактироватьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отменитьToolStripMenuItem,
+            this.повторитьToolStripMenuItem,
+            this.стеретьВсёToolStripMenuItem});
+            this.редактироватьToolStripMenuItem.Image = global::_16.Properties.Resources.Edit_icon__the_Noun_Project_30184__svg;
+            this.редактироватьToolStripMenuItem.Name = "редактироватьToolStripMenuItem";
+            this.редактироватьToolStripMenuItem.Size = new System.Drawing.Size(145, 24);
+            this.редактироватьToolStripMenuItem.Text = "Редактировать";
+            // 
+            // отменитьToolStripMenuItem
+            // 
+            this.отменитьToolStripMenuItem.Name = "отменитьToolStripMenuItem";
+            this.отменитьToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.отменитьToolStripMenuItem.Text = "Отменить";
+            this.отменитьToolStripMenuItem.Click += new System.EventHandler(this.отменитьToolStripMenuItem_Click);
+            // 
+            // повторитьToolStripMenuItem
+            // 
+            this.повторитьToolStripMenuItem.Name = "повторитьToolStripMenuItem";
+            this.повторитьToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.повторитьToolStripMenuItem.Text = "Повторить";
+            this.повторитьToolStripMenuItem.Click += new System.EventHandler(this.повторитьToolStripMenuItem_Click);
+            // 
+            // стеретьВсёToolStripMenuItem
+            // 
+            this.стеретьВсёToolStripMenuItem.Name = "стеретьВсёToolStripMenuItem";
+            this.стеретьВсёToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.стеретьВсёToolStripMenuItem.Text = "Стереть всё";
+            this.стеретьВсёToolStripMenuItem.Click += new System.EventHandler(this.стеретьВсёToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Image = global::_16.Properties.Resources._2940537_200;
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.mnugeneral);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnugeneral;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Безымянный - Блокнот";
+            this.mnugeneral.ResumeLayout(false);
+            this.mnugeneral.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip mnugeneral;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem редактироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отменитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem повторитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стеретьВсёToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filename = "";
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Сохраняет содержимое richTextBox1 в текстовый файл
+        /// </summary>
+
+        void SaveText(string filename)
+        {
+            StreamWriter f = new StreamWriter(filename, false); foreach (string s in richTextBox1.Lines) f.WriteLine(s); f.Close();
+        }
+
+        /// <summary>
+        /// Выводит короткое имя файла без расширения в заголовок текущего окна
+        /// </summary>
+        void Rename(string filename)
+        {
+            string s = new FileInfo(filename).Name.ToString(); s = s.Remove(s.Length - 4);
+            this.Text = s + " – Блокнот";
+        }
+
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear(); filename = "";
+            this.Text = "Безымянный – Блокнот";
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt",
+            };
+            DialogResult res = ofd.ShowDialog(); if (res != DialogResult.OK) return; filename = ofd.FileName; Rename(filename);
+            StreamReader f = new StreamReader(filename); richTextBox1.Text = f.ReadToEnd();
+            f.Close();
+
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog
+                {
+                    Filter = "Текстовые файлы (*.txt)|*.txt",
+                };
+                DialogResult res = sfd.ShowDialog(); if (res != DialogResult.OK) return; filename = sfd.FileName; Rename(filename);
+            }
+            SaveText(filename);
+
+        }
+
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt",
+            };
+            DialogResult res = sfd.ShowDialog(); if (res != DialogResult.OK) return; filename = sfd.FileName; SaveText(filename); Rename(filename);
+
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void стеретьВсёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Clear(); 
+        }
+
+        private void отменитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void повторитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void mnuFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Исходный код программы вы можете найти по ссылке \n ","О программе" , MessageBoxButtons.OK);
+        }
+    }
+}
